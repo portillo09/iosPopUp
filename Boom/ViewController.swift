bd9d71b4eeb3b218c0c1ead8744181e0cc2c73e1
Omar Portillo <Omar_HD@omars-mac.local>
2015-12-18 20:08:05 -0800

    Initial Commit

---
+++ Boom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boom
+//
+//  Created by Omar Portillo on 12/18/15.
+//  Copyright © 2015 EmP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
